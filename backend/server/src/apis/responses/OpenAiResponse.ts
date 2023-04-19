b4dc805228ae91d1d23ab02713995444223245f8
yosky1011 <yosky1011@gmail.com>
2023-04-19 11:14:11 +0900

    #2 Configulation API Key

--- backend/server/src/apis/responses/OpenAiResponse.ts
+++ backend/server/src/apis/responses/OpenAiResponse.ts
@@ -3,5 +3,11 @@ import {IOpenAiMessage} from "../interface/IOpenAiMessage";
 export class OpenAiResponse {
     private messages: Array<IOpenAiMessage>
 
-    get _messages
+    get _messages(): Array<IOpenAiMessage> {
+        return this._messages
+    }
+
+    set _messages(messages: Array<IOpenAiMessage>) {
+        this.messages = messages
+    }
 }
