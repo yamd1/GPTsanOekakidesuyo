d5390e328fd77520d2c85343f858de7a9b25eb80
yosky1011 <yosky1011@gmail.com>
2023-05-09 08:43:22 +0900

    #9 マルチプロセス化

--- backend/server/src/index.ts
+++ backend/server/src/index.ts
@@ -1,18 +1,34 @@
 import {$log} from "@tsed/common";
-import { PlatformExpress } from "@tsed/platform-express";
+import {PlatformExpress} from "@tsed/platform-express";
+import cluster from "cluster";
 import {Server} from "./Server";
 
 async function bootstrap() {
-  try {
-    const platform = await PlatformExpress.bootstrap(Server);
-    await platform.listen();
+    const processes = process.argv[2] !== undefined ? parseInt(process.argv[2], 10) : 4;
 
-    process.on("SIGINT", () => {
-      platform.stop();
-    });
-  } catch (error) {
-    $log.error({event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack});
-  }
+    try {
+
+        if (cluster.isPrimary) {
+            // メインプロセス
+            for (let i = 0; i < processes; i++) {
+                const subProcess = cluster.fork();
+                console.log(`fork process, pid: ${process.pid}, subPid: ${subProcess.process.pid}`);
+            }
+        } else {
+            // サブプロセス
+            const platform = await PlatformExpress.bootstrap(Server);
+            await platform.listen();
+
+            console.log(`sub-process, pid: ${process.pid}`)
+
+            process.on("SIGINT", () => {
+                platform.stop();
+            });
+        }
+
+    } catch (error) {
+        $log.error({event: "SERVER_BOOTSTRAP_ERROR", message: error.message, stack: error.stack});
+    }
 }
 
 bootstrap();
