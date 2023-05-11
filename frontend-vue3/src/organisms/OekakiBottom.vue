d6a6a139f1d0b813c166ade234cda20fd599acf4
yosky1011 <yosky1011@gmail.com>
2023-05-12 04:59:00 +0900

    #23 各ファイルベース作成、状態管理変数定義

---
+++ frontend-vue3/src/organisms/OekakiBottom.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<H2>OekakiBottom</H2>
+</template>
+
---
+++ frontend-vue3/src/organisms/OekakiTop.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<H2>OekakiTop</H2>
+</template>
+
+
