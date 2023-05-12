7a38400e6dc04627c632ec0914569b1f214865d1
yosky1011 <yosky1011@gmail.com>
2023-05-12 10:19:34 +0900

    #27 OekakiTop 作成

--- frontend-vue3/src/organisms/OekakiTop.vue
+++ frontend-vue3/src/organisms/OekakiTop.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+import { toRefs } from "vue"
+
+// pages/Oekaki.vueから受け取る状態管理変
+interface Props {
+    theme: string
+    session: string
+    sessionList: Array<string>
+}
+
+// 状態管理変数のデフォルト値を設定
+/* const props = widthDefaults<Props>(defineProps<Props>(), { */
+/*     theme: "a", */
+/*     session: "b", */
+/*     sessionList: ["c"] */
+/* }) */
+const props = defineProps<Props>()
+
+const { theme, session, sessionList } = toRefs(props)
+
+// 変更時にpages/Oekaki.vueに通知するためのイベント
+const emit = defineEmits(["update:theme", "update:session", "update:sessionList"])
+
 </script>
 
 <template>
 <H2>OekakiTop</H2>
+{{theme.value}}
+{{session}}
+{{sessionList.value}}
+
+
 </template>
 
 
--- frontend-vue3/src/pages/Oekaki.vue
+++ frontend-vue3/src/pages/Oekaki.vue
@@ -1,59 +1,60 @@
 <script setup lang="ts">
+import { ref, computed } from "vue";
 import OekakiTop from "../organisms/OekakiTop.vue";
 import OekakiMiddle from "../organisms/OekakiMiddle.vue";
 import OekakiBottom from "../organisms/OekakiBottom.vue";
 
 // お題を管理する状態管理変数
 const theme = ref("")
-const themeComputed = {
+const themeComputed = computed({
     get: () => theme.value,
     set: (value: string) => {
         theme.value = value
     }
-}
+})
 
 // 選択されたセッションを保持する状態管理変数
 const session = ref("")
-const sessionComputed = {
+const sessionComputed = computed({
     get: () => session.value,
     set: (value: string) => {
         session.value = value
     }
-}
+})
 
 // 過去のセッション一覧を保持する状態管理変数
 const sessionList = ref([])
-const sessionListComputed = {
+const sessionListComputed = computed({
     get: () => sessionList.value,
     set: (value: Array<string>) => {
         sessionList.value = value
     }
-}
+})
 
 // 入力されたバイナリを保持する状態管理変数
 const binaries = ref("")
-const binariesComputed = {
+const binariesComputed = computed({
     get: () => binaries.value,
     set: (value: string) => {
         binaries.value = value
     }
-}
+})
 
 // OpenAiから返却された結果を保持する状態管理変数
 const openAiResult = ref("")
-const openAiResultComputed = {
+const openAiResultComputed = computed({
     get: () => openAiResult.value,
     set: (value: string) => {
         openAiResult.value = value
     }
-}
+})
 
 </script>
 
 <template>
 <H1>Hello Pages</H1>
-<OekakiTop />
-<OekakiMiddle />
-<OekakiBottom />
+<OekakiTop :theme="themeComputed" :session="sessionComputed" :sessionList="sessionListComputed"/>
+<OekakiMiddle :binaries="binariesComputed"/>
+<OekakiBottom :binaries="binariesComputed" :openAiResult="openAiResultComputed"/>
 </template>
 
