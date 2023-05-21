20738269dfa18da40e2e0093ee387cf0412a7d7c
yosky1011 <yosky1011@gmail.com>
2023-05-21 21:07:42 +0900

    #34 PostSessionAPI(OpenAi連携)へのリクエスト処理実装

---
+++ frontend-vue3/src/molecules/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+interface Props {
+    event: () => void
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    event: () => { }
+})
+
+const { event } = toRefs(props)
+
+const onClick = () => {
+    event.value()
+}
+
+</script>
+
+<template>
+    <div>
+        <button @click="onClick">完成</button>
+    </div>
+</template>
